3067f6d56f09257eabbd2c903c01860e6c24d3f8
Pinto <pinto702rodri@gmail.co>
2025-10-20 12:52:43 -0500

    .sparkles:

--- Taller2/script.js
+++ Taller2/script.js
@@ -1,15 +1,58 @@
 // ¿Cuántos municipios comienzan con "La" y cuál es la cantidad total incautada en ellos?//
 
-db.incautaciones.aggregate([
+/*
+db.Municipio.aggregate([
     {
         $match: {
-            Municipio: {$regex:"^(la)", $options: "i"  }
+            nombre: { $regex: "^(la)", $options: "i" }
 
         }
-    },
-    {$group: {
-        _id: "$cod_muni",
-        cantidad: {$sum: "$cantidad"}
     }
-}
 ]);
+
+db.incautaciones.aggregate([
+    {
+        $group: {
+            _id: "$id_municipio",
+            cantidad: { $sum: "$cantidad" }
+        }
+    }
+]);*/
+
+db.incautaciones.aggregate([
+    {
+        $lookup: {
+          from: "Municipio",
+          localField: "id_municipio",
+          foreignField: "id_municipio",
+          as: "nombre_municipio" 
+        }
+    },
+    {
+        $unwind: "$nombre_municipio"
+    },
+    {
+        $match: {
+            "nombre_municipio.nombre": {  $regex: "^(la)", $options: "i" }
+        }
+    },
+    {
+        $group: {
+            _id: null,
+            cantidad: { $sum: "$cantidad"},
+            totalMunicipios: {$sum: 1}
+        }
+    }
+]);
+
+
+//Top 5 departamentos donde los municipios terminan en "al" y la cantidad incautada.//
+
+db.Municipio.aggregate([
+    {
+        $match:{
+            nombre: {$regex: "(al)$", $options: "i"}
+        }
+    }
+]);
+
